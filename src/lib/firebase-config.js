b1a0fb1765bb07aee0f212bc1e380137ac3b238a
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2023-08-11 15:37:49 +0530

    Hello! Mom

--- src/lib/firebase-config.js
+++ src/lib/firebase-config.js
@@ -2,11 +2,11 @@
 
 export const FIREBASE_CONFIG = {
   apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  authDomain: "the-abyss-78a1b.firebaseapp.com",
+  projectId: "the-abyss-78a1b",
+  storageBucket: "the-abyss-78a1b.appspot.com",
+  messagingSenderId: "622000397281",
+  appId: "1:622000397281:web:e35affab1fe6c02afa50a0"
 }
 
 export default FIREBASE_CONFIG
