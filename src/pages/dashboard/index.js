78fe496b72e4bd8ce090a8847436a06b8b806f75
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2023-08-12 15:15:27 +0530

    Hello! Mom

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -85,7 +85,7 @@ export default function Dashboard() {
                 margin-top: 2.5rem;
               `}
             >
-              Free your mind ✨ Why don&apos;t you check out some suggested <a href="https://theabyss.ink">topics</a> 🤔?
+              Free your mind ✨ Why don&apos;t you check out some suggested <a href="https://www.theabyss.ink/solomonlijo/thefirstpost">topics</a> 🤔?
             </p>
           ) : (
             <ul
