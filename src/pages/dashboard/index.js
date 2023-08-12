290daacc9a8d6f27ce22c6eb3d01ebf838cdbd07
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2023-08-12 10:50:13 +0530

    Hello! Mom

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -82,7 +82,7 @@ export default function Dashboard() {
                 margin-top: 2.5rem;
               `}
             >
-              You have no posts yet 😔
+              Free your mind ✨
             </p>
           ) : (
             <ul
