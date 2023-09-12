710c77e6f6c45472d54a2a814db03024d4b3b20a
Solomon Shalom Lijo <71135230+solomonshalom@users.noreply.github.com>
2023-09-12 16:27:20 +0530

    Hello! Mom

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,13 +1,14 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
-import FIREBASE_CONFIG from './firebase-config'
+import FIREBASE_CONFIG from './firebase-config';
 
 if (firebase.apps.length === 0) {
-  firebase.initializeApp(FIREBASE_CONFIG)
+  firebase.initializeApp(FIREBASE_CONFIG);
 }
 
-export const auth = firebase.auth()
-export const firestore = firebase.firestore()
-export default firebase
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export { auth, firestore, firebase };
