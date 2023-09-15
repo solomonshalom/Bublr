1918a7d00dd2761055e95d7ba7c006f729df993e
Jennifer Shin <jenmomo@Jennifers-MacBook-Pro.local>
2023-09-15 12:25:29 -0700

    finished search bar for posts

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import Link from 'next/link'
 import Head from 'next/head'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { css } from '@emotion/react'
 import { useRouter } from 'next/router'
 import { htmlToText } from 'html-to-text'
@@ -15,6 +15,7 @@ import Button from '../../components/button'
 import Header from '../../components/header'
 import Spinner from '../../components/spinner'
 import Container from '../../components/container'
+import Input from '../../components/input'
 import ProfileSettingsModal from '../../components/profile-settings-modal'
 
 function formatDate(date) {
@@ -36,6 +37,12 @@ export default function Dashboard() {
     firestore.collection('posts').where('author', '==', user ? user.uid : ''),
     { idField: 'id' },
   )
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+
+  useEffect(() => {
+    setFilteredPosts(posts)
+  }, posts)
 
   useEffect(() => {
     console.log(user, userLoading, userError)
@@ -44,6 +51,19 @@ export default function Dashboard() {
       return
     }
   }, [router, user, userLoading, userError])
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (posts) filterPosts();
+    }, 500)
+
+    return () => clearTimeout(delayDebounceFn)
+  }, [searchInput])
+
+  const filterPosts = () => {
+    let tempPosts = posts.filter(p => p.title.includes(searchInput))
+    setFilteredPosts(tempPosts);
+  }
 
   return (
     <>
@@ -69,7 +89,7 @@ export default function Dashboard() {
           <p>Oop, we&apos;ve had an error:</p>
           <pre>{JSON.stringify(error)}</pre>
         </>
-      ) : user && posts ? (
+      ) : user && filteredPosts && posts ? (
         <>
           <Button
             outline
@@ -106,7 +126,7 @@ export default function Dashboard() {
             </Button>
           </Link>
 
-          {posts.length === 0 ? (
+          { posts?.length === 0 && !filteredPosts ? (
             <p
               css={css`
                 margin-top: .1rem;
@@ -115,79 +135,100 @@ export default function Dashboard() {
               Welcome to the Abyss! 🔥 A Beautiful Place to Free your Mind ✨
             </p>
           ) : (
-            <ul
-              css={css`
-                margin-top: 0rem;
-                list-style: none;
-              `}
-            >
-              {[...posts]
-                .sort(
-                  (a, b) =>
-                    b.lastEdited.toDate().getTime() -
-                    a.lastEdited.toDate().getTime(),
-                )
-                .map(post => (
-                  <li
-                    key={post.id}
-                    css={css`
-                      margin: 2rem 0;
-                      display: flex;
+            <div>
+              <Input
+                id="search-posts"
+                type="text"
+                value={searchInput}
+                placeholder="Search your posts..."
+                onChange={e => {
+                  setSearchInput(e.target.value);
+                }}
+              />
+              { filteredPosts?.length === 0 && searchInput.length > 0 ? (
+              <p
+                css={css`
+                  margin-top: 2rem;
+                `}
+              >
+                No posts matched your search query...
+              </p>
+              ) : (
+                <ul
+                  css={css`
+                    margin-top: 0rem;
+                    list-style: none;
+                  `}
+                >
+                  {[...filteredPosts]
+                    .sort(
+                      (a, b) =>
+                        b.lastEdited.toDate().getTime() -
+                        a.lastEdited.toDate().getTime(),
+                    )
+                    .map(post => (
+                      <li
+                        key={post.id}
+                        css={css`
+                          margin: 2rem 0;
+                          display: flex;
 
-                      a {
-                        width: 70%;
-                        margin-left: auto;
-                        display: inline-block;
-                        text-decoration: none;
-                        color: inherit;
-                      }
+                          a {
+                            width: 70%;
+                            margin-left: auto;
+                            display: inline-block;
+                            text-decoration: none;
+                            color: inherit;
+                          }
 
-                      @media (max-width: 720px) {
-                        display: block;
-                        margin: 2rem 0;
+                          @media (max-width: 720px) {
+                            display: block;
+                            margin: 2rem 0;
 
-                        a {
-                          width: 100%;
-                          margin: 0;
-                        }
+                            a {
+                              width: 100%;
+                              margin: 0;
+                            }
 
-                        p {
-                          margin-bottom: 0.75rem;
-                        }
-                      }
-                    `}
-                  >
-                    <p
-                      css={css`
-                        font-size: 0.9rem;
-                        color: var(--grey-3);
-                      `}
-                    >
-                      <time>{formatDate(post.lastEdited.toDate())}</time>
-                    </p>
-                    <Link href={`/dashboard/${post.id}`}>
-                      <a>
-                        {!post.published && (
-                          <span
-                            css={css`
-                              display: inline-block;
-                              background: var(--grey-2);
-                              color: var(--grey-4);
-                              opacity: 0.7;
-                              padding: 0.25rem;
-                              border-radius: 0.25rem;
-                              font-size: 0.9rem;
-                            `}
-                          >
-                            DRAFT
-                          </span>
-                        )}{' '}
-                        {post.title ? htmlToText(post.title) : 'Untitled'}
-                      </a>
-                    </Link>
-                  </li>
-                ))}
-            </ul>
+                            p {
+                              margin-bottom: 0.75rem;
+                            }
+                          }
+                        `}
+                      >
+                        <p
+                          css={css`
+                            font-size: 0.9rem;
+                            color: var(--grey-3);
+                          `}
+                        >
+                          <time>{formatDate(post.lastEdited.toDate())}</time>
+                        </p>
+                        <Link href={`/dashboard/${post.id}`}>
+                          <a>
+                            {!post.published && (
+                              <span
+                                css={css`
+                                  display: inline-block;
+                                  background: var(--grey-2);
+                                  color: var(--grey-4);
+                                  opacity: 0.7;
+                                  padding: 0.25rem;
+                                  border-radius: 0.25rem;
+                                  font-size: 0.9rem;
+                                `}
+                              >
+                                DRAFT
+                              </span>
+                            )}{' '}
+                            {post.title ? htmlToText(post.title) : 'Untitled'}
+                          </a>
+                        </Link>
+                      </li>
+                    ))}
+                </ul>
+              )}
+            </div>
           )}
         </>
       ) : (
