8d675d95dea8b3c4c2f5b8d06a81adbc9cd7b65b
Solomon Shalom Lijo <71135230+solomonshalom@users.noreply.github.com>
2023-10-04 10:41:35 +0530

    Hello! Mom

--- src/components/AnonymousLoginButton.js
+++ src/components/AnonymousLoginButton.js
@@ -1,29 +1,36 @@
 /** @jsxImportSource @emotion/react */
-import Button, { LinkButton } from '../components/button'
-import firebase, { auth } from '../lib/firebase'
-import { setUser, userWithIDExists } from '../lib/db'
+import Button from '../components/button';
+import firebase, { auth } from '../lib/firebase';
+import { setUser, userWithIDExists } from '../lib/db';
 
 const AnonymousLoginButton = () => {
+  const avatarStyles = ['big-smile', 'lorelei'];
+
   const generateRandomSeed = () => {
-    // Generate a random seed here
     return Math.floor(Math.random() * 1000000).toString();
+  };
+
+  const getRandomStyle = () => {
+    const randomIndex = Math.floor(Math.random() * avatarStyles.length);
+    return avatarStyles[randomIndex];
   };
 
   const handleAnonymousLogin = async () => {
     const randomSeed = generateRandomSeed();
+    const randomStyle = getRandomStyle();
 
     auth.signInAnonymously().then(async (cred) => {
       const userExists = await userWithIDExists(cred.user.uid);
 
       if (!userExists) {
-        // Create a new user with the generated avatar seed
+        // Create a new user with the generated avatar seed and style
         await setUser(cred.user.uid, {
           name: cred.user.uid,
           displayName: 'Anonymous',
           about: 'Say something about yourself 😃 or 🔫',
           posts: [],
           readingList: [],
-          photo: `https://api.dicebear.com/7.x/lorelei/svg?seed=${randomSeed}`, // Include the random seed in the URL
+          photo: `https://api.dicebear.com/7.x/${randomStyle}/svg?seed=${randomSeed}`,
         });
       }
     });
