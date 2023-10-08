895a829ee9a9490ce97783c2189e2b717ecbea0a
Solomon Shalom Lijo <71135230+solomonshalom@users.noreply.github.com>
2023-10-08 15:33:18 +0530

    Hello! Mom

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -7,10 +7,48 @@ function ToggleTheme() {
     setTheme(theme === 'light' ? 'dark' : 'light');
   };
 
+  const toggleSwitchStyles = {
+    position: 'relative',
+    display: 'inline-block',
+    width: '60px',
+    height: '30px',
+  };
+
+  const sliderStyles = {
+    position: 'absolute',
+    cursor: 'pointer',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    backgroundColor: '#ccc',
+    borderRadius: '15px',
+    transition: '0.4s',
+  };
+
+  const sliderBeforeStyles = {
+    position: 'absolute',
+    content: '',
+    height: '26px',
+    width: '26px',
+    left: '4px',
+    bottom: '4px',
+    backgroundColor: 'white',
+    borderRadius: '50%',
+    transition: '0.4s',
+  };
+
   return (
-    <button onClick={toggleTheme} className='toggleButton'>
-      Toggle Theme
-    </button>
+    <label style={toggleSwitchStyles} className='toggleSwitch'>
+      <input
+        type='checkbox'
+        checked={theme === 'dark'}
+        onChange={toggleTheme}
+      />
+      <span style={sliderStyles} className='slider'>
+        <span style={sliderBeforeStyles}></span>
+      </span>
+    </label>
   );
 }
 
