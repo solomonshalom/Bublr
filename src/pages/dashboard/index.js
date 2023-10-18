7eecd79806fce4cb18fa46fe38ade70ac5a3fc9a
Solomon Shalom Lijo <solomonlijo@skiff.com>
2023-10-18 17:20:13 +0530

    Hello! Mom
    Update index.js w/ fix for SVG

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -50,7 +50,7 @@ export default function Dashboard() {
   // Set initial filteredPosts
   useEffect(() => {
     setFilteredPosts(posts)
-  }, posts)
+  }, [posts])
 
   // Get the filtered posts from Search component
   const getFilteredPosts = (fp) => {
@@ -107,9 +107,26 @@ export default function Dashboard() {
               router.push(`/dashboard/${newPostsId}`)
             }}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="21px" height="21px" fill="none" stroke-width="1.5" viewBox="0 0 24 24" color="#ffffff" css={css`
-                margin: 0.2em 0 0 0.1em
-              `}><path stroke="#ffffff" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" d="m14.363 5.652 1.48-1.48a2 2 0 0 1 2.829 0l1.414 1.414a2 2 0 0 1 0 2.828l-1.48 1.48m-4.243-4.242-9.616 9.615a2 2 0 0 0-.578 1.238l-.242 2.74a1 1 0 0 0 1.084 1.085l2.74-.242a2 2 0 0 0 1.24-.578l9.615-9.616m-4.243-4.242 4.243 4.242"></path>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="21px"
+              height="21px"
+              fill="none"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              css={css`
+                margin: 0.2em 0 0 0.1em;
+                /* Adjust color for the SVG based on user color scheme */
+                stroke: var(--svg-color);
+              `}
+            >
+              <path
+                stroke="var(--svg-color)"
+                stroke-width="1.3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m14.363 5.652 1.48-1.48a2 2 0 0 1 2.829 0l1.414 1.414a2 2 0 0 1 0 2.828l-1.48 1.48m-4.243-4.242-9.616 9.615a2 2 0 0 0-.578 1.238l-.242 2.74a1 1 0 0 0 1.084 1.085l2.74-.242a2 2 0 0 0 1.24-.578l9.615-9.616m-4.243-4.242 4.243 4.242"
+              ></path>
             </svg>
           </Button>
 
@@ -138,9 +155,26 @@ export default function Dashboard() {
                 text-align: center;
               `}
               >
-              <svg xmlns="http://www.w3.org/2000/svg" width="1.1em" height="1.1em" fill="none" stroke-width="1.5" viewBox="0 0 24 24" color="#ffffff"css={css`
-                  margin: 0.25em 0 0 0.05em
-                `}><path stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M7.9 8.08c0-4.773 7.5-4.773 7.5 0 0 3.409-3.409 2.727-3.409 6.818M12 19.01l.01-.011"></path>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.1em"
+                height="1.1em"
+                fill="none"
+                stroke-width="1.5"
+                viewBox="0 0 24 24"
+                css={css`
+                  margin: 0.25em 0 0 0.05em;
+                  /* Adjust color for the SVG based on user color scheme */
+                  stroke: var(--svg-color);
+                `}
+              >
+                <path
+                  stroke="var(--svg-color)"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M7.9 8.08c0-4.773 7.5-4.773 7.5 0 0 3.409-3.409 2.727-3.409 6.818M12 19.01l.01-.011"
+                ></path>
               </svg>
             </Button>
           </Link>
